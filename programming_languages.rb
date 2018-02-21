982cbe47b01c5b2efefbb95de7736f9e170d2b01
J.D. Bean <jonathandbean@gmail.com>
2018-02-21 21:23:39 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ require "pry"
 def reformat_languages(languages)
   new_hash = {}
   languages.each do |oo_or_func, languages_hash|
-    attributes_hash.each do |language, attributes_hash|
+    languages_hash.each do |language, attributes_hash|
       binding.pry
     end
   binding.pry
